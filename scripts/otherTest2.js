1c2e3e5d047d3d97f153ce5a73169782d9703eeb
Franco Ojeda <fojeda@slingr.io>
2023-05-01 18:18:11 -0300

    adds dep method

--- scripts/otherTest2.js
+++ scripts/otherTest2.js
@@ -1,3 +1,8 @@
 exports.otherTest2 = function () {
     return "other test 2";
 }
+
+exports.nestedmethod = function () {
+    var value = dependencies.nesteddep.test.nested();
+    return value;
+}
