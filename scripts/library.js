484ad5a89d78b392e432cd5c02e6fa01f5fcd2a3
Franco Ojeda <fojeda@slingr.io>
2023-05-17 16:02:40 -0300

    fixes dependency name

--- scripts/library.js
+++ scripts/library.js
@@ -3,7 +3,7 @@ exports.packageName = function () {
 }
 
 exports.nested1 = function () {
-    var versionResponse = "Version response: " + dependencies.versionTest.versionLib().versionValue();
-    var dependencyResponse = "Dependency response: " + dependencies.nesteddep1.other().nested1()
+    var versionResponse = "Version response: " + dependencies.versionTest.versionLib.versionValue();
+    var dependencyResponse = "Dependency response: " + dependencies.nesteddep1.other.nested1()
     return "From package [testLibrary] |\n " + dependencyResponse + " |\n " + versionResponse;
 }
